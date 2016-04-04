0044f76e1d95b3a946957ee44aee43651913a2b3
Brandon Newton <bitwiselove@gmail.com>
2016-04-04 11:56:46 -0700

    Compile and concatente SCSS using Gulp

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+
+gulp.task('scss', () => {
+  gulp.src('scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('compiled.css'))
+    .pipe(gulp.dest('dist/css/'))
+});
